0ce2db86b310debe7151887b67b66ad2401df1e7
Matilde Morrone <contact@morrone.dev>
2024-07-18 15:59:07 +0200

    Link to the correct libudev version

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 fn main() {
-    // pkg_config::Config::new()
-    //     // .atleast_version("1.26.1") // FIXME find an appropriate version
-    //     .probe("libudev")
-    //     .expect("Failed to link to libudev");
+    pkg_config::Config::new()
+        .atleast_version("221")
+        .probe("libudev")
+        .expect("Failed to link to libudev");
 
-    pkg_config::probe_library("libudev").expect("Failed to link libudev");
+    // pkg_config::probe_library("libudev").expect("Failed to link libudev");
 }
