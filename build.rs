d4a929d66b1298be6c237d7613e803504c8a652d
Matilde Morrone <contact@morrone.dev>
2025-01-09 19:15:05 +0100

    Update generated bindings

--- build.rs
+++ build.rs
@@ -3,6 +3,4 @@ fn main() {
         .atleast_version("221")
         .probe("libudev")
         .expect("Failed to link to libudev");
-
-    // pkg_config::probe_library("libudev").expect("Failed to link libudev");
 }
